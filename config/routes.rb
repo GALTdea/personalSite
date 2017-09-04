e65018d1101891c19b0dea671d38eff2297a3b6f
GALTdea <gustavovalenzuela1180@gmail.com>
2017-09-04 14:27:58 -0700

    added secret file that somehow got deleted in previews pushes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :portfolios
-  resources :blogs
 
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  resources :blogs
 
 end
