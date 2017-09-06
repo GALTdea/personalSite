ac99a85da1dad8050d31768fb9abebe162be634c
GALTdea <gustavovalenzuela1180@gmail.com>
2017-09-05 20:30:28 -0700

    implemented default values from model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,4 +11,19 @@ class Portfolio < ApplicationRecord
 
   # 2.
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+
+
+
+  #call back
+  # after the portolio item is instantiated in the new action
+  after_initialize :set_defaults
+
+
+  # the ||= operator allows us to only give the values if its nil, so when an update happens it will
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumb_image ||= 'http://via.placeholder.com/350x200'
+  end
 end
+
+
