e24fceb73d12b9d3c665a8672425edbc17e4d4d0
GALTdea <gustavovalenzuela1180@gmail.com>
2017-09-17 19:51:08 -0700

    styled blog index

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   #sets the css layout  according to whatever we chose,
   layout 'blog'
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
